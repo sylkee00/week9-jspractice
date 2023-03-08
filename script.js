2aecf3be084d7e9317930d41b9f5d8307fd80632
Suyeon Lee <slee1055@my.bcit.ca>
2023-03-08 12:52:13 -0800

    js practice done

---
+++ script.js
@@ -0,0 +1,14 @@
+let result = "equal";
+let isEqual = "false";
+const num1 = 1;
+const num2 = 1;
+
+if(num1 === num2){
+    isEqual = true;
+}
+
+if(isEqual === true){
+    console.log("The two numbers are " + result + "!");
+}else{
+    console.log("The two numbers are NOT " + result + "!");
+}
